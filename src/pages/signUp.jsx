bd9b1ee1a4ac191b9467fb78a7d9ba5558b60605
shivamKcode <shivambaniya246@gmail.com>
2023-12-30 13:12:16 -0500

    fixed deployment issue

--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Card from "../components/Card";
 import Button from "../components/Button";
 import Input from "../components/Input";
-import Eye from "../assets/icon-hide-sideBar.svg";
+import Eye from "../assets/icon-hide-sidebar.svg";
 import EyeIcon from "../assets/icon-show-sidebar.svg";
 import Cross from "../assets/icon-cross.svg";
 
