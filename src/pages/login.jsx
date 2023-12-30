6249ab314632cb48fff771beaf4548a3eb66913c
shivamKcode <shivambaniya246@gmail.com>
2023-12-30 13:11:13 -0500

    fixed deployment issue

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,7 +3,7 @@ import Card from "../components/Card";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import Input from "../components/Input";
-import Eye from "../assets/icon-hide-sideBar.svg";
+import Eye from "../assets/icon-hide-sidebar.svg";
 import EyeIcon from "../assets/icon-show-sidebar.svg";
 import SignupPage from "./signUp";
 
